fb3c9e8e68e501cfec514b0b9d847e24e5ae8a76
Rimil Dey <rimildeyjsr@gmail.com>
2020-03-28 13:54:34 +0530

    board game structure + divs changing turns + showing x and 0 plus storing state

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, SimpleChanges, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'app-game-board',
   templateUrl: './game-board.component.html',
   styleUrls: ['./game-board.component.css']
 })
-export class GameBoardComponent implements OnInit {
+export class GameBoardComponent implements OnInit, OnChanges {
 
+  @Input() noOfBoxes: number;
+  @Input() currentPlayer: number;
+  gameBoardArr = {};
+  numberArray = [];
+  cell = null;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+    if (changes.noOfBoxes && this.noOfBoxes) {
+      for (let i = 0; i < this.noOfBoxes; i++) {
+        this.numberArray[i] = i;
+        for (let j = 0; j < this.noOfBoxes; j++) {
+          const key = i + '-' + j;
+          this.gameBoardArr[key] = null;
+        }
+      }
+    }
+
+    if (changes.currentPlayer) {
+      this.cell = event.target;
+      this.cell['innerHTML'] = this.currentPlayer ? 'X' : 'O';
+      this.gameBoardArr[this.cell['dataset']['key']] = this.currentPlayer;
+      this.cell = null;
+    }
+  }
+
+  getKey(i, j) {
+    return i + '-' + j;
+  }
+
+  registerState(event) {
+    if (this.cell) {
+      let previous = this.cell;
+      previous['innerHTML'] = '';
+      this.gameBoardArr[previous['dataset']['key']] = null;
+    }
+
+    this.cell = event.target;
+    this.cell['innerHTML'] = this.currentPlayer ? 'X' : 'O';
+    this.gameBoardArr[this.cell['dataset']['key']] = this.currentPlayer;
+    console.log(this.gameBoardArr);
   }
 
 }
